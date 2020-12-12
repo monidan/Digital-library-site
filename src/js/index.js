1644280441ae58813e684b897172b989cc37a9c2
monidan <daniil17032002@gmail.com>
2020-12-12 02:53:08 +0200

    Made functionality of two carousels.

---
+++ src/js/index.js
@@ -0,0 +1,187 @@
+// LIBRARY Carousel '#r2__library'
+
+(function(){
+  'use strict';
+
+  let leftCounter = 3,
+      rightCounter = 0;
+
+  let translateXProps = {
+    percentage: 0,
+    rem: 0
+  }
+
+  const LEFT_MAX = 3;
+  const RIGHT_MAX = 3; 
+
+  const PERCANTAGE_STEP = 100;
+  const REM_STEP = 1;
+
+  const mainEl = document.querySelector('#r2__library');
+  const carouselItemsEls = mainEl.querySelectorAll('.r2__library__carousel__item');
+
+  const leftArrowEl = mainEl.querySelector('.r2__carousel__arrow_left');
+  const rightArrowEl = mainEl.querySelector('.r2__carousel__arrow_right');
+
+  const slideCarousel = e => {
+    if(isArrowButtonInActive(e.target)) return;
+
+    const direction = getDirection(e.target.classList);
+    if(!direction) return;
+
+    if(direction === 'left') {
+      rightArrowEl.classList.remove('r2__arrow__button__inactive')
+
+      if(checkLimit(direction)) return;
+      leftCounter++;
+      rightCounter--;
+
+      translateXProps.percentage -= PERCANTAGE_STEP;
+      translateXProps.rem -= REM_STEP;
+
+      carouselItemsEls.forEach(carouselItem => {
+        carouselItem.style.transform = `translateX(calc(-${translateXProps.percentage}% - ${translateXProps.rem}rem))`;
+      })
+
+      if(checkLimit(direction)){
+        leftArrowEl.classList.add('r2__arrow__button__inactive')
+      }
+      console.log('left Counter', translateXProps)
+    } else {
+      leftArrowEl.classList.remove('r2__arrow__button__inactive')
+
+      if(checkLimit(direction)) return;
+      rightCounter++;
+      leftCounter--;
+
+      translateXProps.percentage += PERCANTAGE_STEP;
+      translateXProps.rem += REM_STEP;
+
+      carouselItemsEls.forEach(carouselItem => {
+        carouselItem.style.transform = `translateX(calc(-${translateXProps.percentage}% - ${translateXProps.rem}rem))`;
+      })
+
+      if(checkLimit(direction)){
+        rightArrowEl.classList.add('r2__arrow__button__inactive');
+      }
+      console.log('right Counter', translateXProps)
+    }
+  }
+
+  const getDirection = className => {
+    if (!className) return;
+
+    return className.contains('r2__carousel__arrow_left') ? 
+      'left'
+      : 'right';
+  }
+
+  const checkLimit = direction => {
+    if(direction === 'left') {
+      return leftCounter === LEFT_MAX;
+    }else {
+      return rightCounter === RIGHT_MAX;
+    }
+  }
+
+  const isArrowButtonInActive = button => {
+    return button.classList.contains('r2__arrow__button__inactive');
+  }
+
+  leftArrowEl.addEventListener('click', slideCarousel);
+  rightArrowEl.addEventListener('click', slideCarousel);
+})();
+
+// ARTICLES Carousel '#r2__popular__articles'
+
+(function(){
+  'use strict';
+
+  let leftCounter = 3,
+      rightCounter = 0;
+
+  let translateXProps = {
+    percentage: 0,
+    rem: 0
+  }
+
+  const LEFT_MAX = 3;
+  const RIGHT_MAX = 3; 
+
+  const PERCANTAGE_STEP = 100;
+  const REM_STEP = 1;
+
+  const mainEl = document.querySelector('#r2__popular__articles');
+  const carouselItemsEls = mainEl.querySelectorAll('.r2__popular__carousel__item');
+
+  const leftArrowEl = mainEl.querySelector('.r2__carousel__arrow_left');
+  const rightArrowEl = mainEl.querySelector('.r2__carousel__arrow_right');
+
+  const slideCarousel = e => {
+    if(isArrowButtonInActive(e.target)) return;
+
+    const direction = getDirection(e.target.classList);
+    if(!direction) return;
+
+    if(direction === 'left') {
+      rightArrowEl.classList.remove('r2__arrow__button__inactive')
+
+      if(checkLimit(direction)) return;
+      leftCounter++;
+      rightCounter--;
+
+      translateXProps.percentage -= PERCANTAGE_STEP;
+      translateXProps.rem -= REM_STEP;
+
+      carouselItemsEls.forEach(carouselItem => {
+        carouselItem.style.transform = `translateX(calc(-${translateXProps.percentage}% - ${translateXProps.rem}rem))`;
+      })
+
+      if(checkLimit(direction)){
+        leftArrowEl.classList.add('r2__arrow__button__inactive')
+      }
+      console.log('left Counter', translateXProps)
+    } else {
+      leftArrowEl.classList.remove('r2__arrow__button__inactive')
+
+      if(checkLimit(direction)) return;
+      rightCounter++;
+      leftCounter--;
+
+      translateXProps.percentage += PERCANTAGE_STEP;
+      translateXProps.rem += REM_STEP;
+
+      carouselItemsEls.forEach(carouselItem => {
+        carouselItem.style.transform = `translateX(calc(-${translateXProps.percentage}% - ${translateXProps.rem}rem))`;
+      })
+
+      if(checkLimit(direction)){
+        rightArrowEl.classList.add('r2__arrow__button__inactive');
+      }
+      console.log('right Counter', translateXProps)
+    }
+  }
+
+  const getDirection = className => {
+    if (!className) return;
+
+    return className.contains('r2__carousel__arrow_left') ? 
+      'left'
+      : 'right';
+  }
+
+  const checkLimit = direction => {
+    if(direction === 'left') {
+      return leftCounter === LEFT_MAX;
+    }else {
+      return rightCounter === RIGHT_MAX;
+    }
+  }
+
+  const isArrowButtonInActive = button => {
+    return button.classList.contains('r2__arrow__button__inactive');
+  }
+
+  leftArrowEl.addEventListener('click', slideCarousel);
+  rightArrowEl.addEventListener('click', slideCarousel);
+})()
